7820130c974af131e91cf8225386a42600cc3f52
J Leadbetter <jleadbetter@laterpay.net>
2021-04-18 11:53:46 +0200

    Docker compose cleanup

--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -6,6 +6,24 @@
 #                                                                             #
 ###############################################################################
 
-gunicorn demo_backend.asgi:application \
+function usage {
+  echo "./$(basename $0) [-b]"
+  echo "Include the -b flag to also build the project."
+}
+
+while getopts ":b" arg; do
+  case $arg in
+    b)
+      scripts/build.sh
+      ;;
+    ?)
+      echo "Invalid option."
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+exec gunicorn demo_backend.asgi:application \
     -k uvicorn.workers.UvicornWorker \
     --bind 0.0.0.0:8000
