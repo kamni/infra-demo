7dad20b3f6b266b52e936cd56e70dc2707f74b0b
J Leadbetter <jleadbetter@laterpay.net>
2021-04-18 06:42:43 +0200

    Working on nginx config; started circleci file

--- backend/demo_backend/settings.py
+++ backend/demo_backend/settings.py
@@ -22,6 +22,7 @@ DEBUG = os.environ['DJANGO_DEBUG']
 
 ALLOWED_HOSTS = os.environ['DJANGO_ALLOWED_HOSTS']
 
+# TODO: logging
 
 # Application definition
 
@@ -111,6 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/opt/jleadbetter/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
