4cf3ad8ec0577d0205d089a5a6e0cf877fd0e2b9
J Leadbetter <jleadbetter@laterpay.net>
2021-04-18 09:52:03 +0200

    Django collectstatic works

--- backend/demo_backend/settings.py
+++ backend/demo_backend/settings.py
@@ -111,8 +111,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/opt/jleadbetter/static'
+STATIC_URL = os.environ['DJANGO_STATIC_URL']
+STATIC_ROOT = os.environ['DJANGO_STATIC_ROOT']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
